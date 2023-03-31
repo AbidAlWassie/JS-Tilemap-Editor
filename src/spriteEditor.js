5928bc755ec3b36742920a8741274d735da16b46
Abid Al Wassie <abidalwassie@outlook.com>
2023-03-31 22:43:38 +0600

    fixed image stretching issue

--- src/spriteEditor.js
+++ src/spriteEditor.js
@@ -1,13 +1,20 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('spriteCanvas');
 canvas.style.imageRendering = "pixelated";
 const ctx = canvas.getContext('2d');
 const imageUpload = document.getElementById('imageUpload');
 const tileWidth = 16;
 const tileHeight = 16;
 
-const container = document.getElementById('container');
-canvas.width = container.offsetWidth;
-canvas.height = container.offsetHeight;
+const container = document.getElementById('imgContainer');
+
+// function to scale canvas to fit screen
+function scaleCanvas() {
+  const scaleFactor = Math.min(window.innerWidth / canvas.width, window.innerHeight / canvas.height);
+  canvas.style.width = canvas.width * scaleFactor + 'px';
+  canvas.style.height = canvas.height * scaleFactor + 'px';
+}
+
+window.addEventListener('resize', scaleCanvas);
 
 imageUpload.addEventListener('change', (event) => {
   const file = event.target.files[0];
@@ -28,9 +35,14 @@ imageUpload.addEventListener('change', (event) => {
           const x = col * tileWidth;
           const y = row * tileHeight;
           ctx.drawImage(img, col * tileWidth, row * tileHeight, tileWidth, tileHeight, x, y, tileWidth, tileHeight);
+          // ctx.strokeStyle = 'black';
+          // ctx.lineWidth = 1;
+          // ctx.strokeRect(x, y, tileWidth, tileHeight);
         }
       }
+      scaleCanvas(); // call function to scale canvas to fit screen
+      console.log(`The canvas has ${numCols} columns and ${numRows} rows.`);
+      console.log(`There are ${numCols * numRows} tiles in total.`);
     };
   };
 });
-
